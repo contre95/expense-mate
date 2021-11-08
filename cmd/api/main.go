73a12392f25e17a93033e71ec113a19731ea0fa5
Lucas Contreras <lucascontre95@gmail.com>
2021-11-08 23:51:43 +0100

    Fiber v2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,15 +6,15 @@ import (
 	"expenses-app/pkg/gateways/logger"
 	"expenses-app/pkg/gateways/storage/sql"
 	"expenses-app/pkg/presenters/http"
+	"fmt"
 
-	fiber "github.com/gofiber/fiber/v2"
-
+	"github.com/gofiber/fiber/v2"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-
+	fmt.Println("Starting")
 	// SQL Storage
 	db, _ := gorm.Open(sqlite.Open("db/ims.db"), &gorm.Config{})
 	storage := sql.NewStorage(db)
