367308d78adfa779b622fb1b850bf4f32ee6c808
Lucas Contreras <lucascontre95@gmail.com>
2021-11-13 21:57:49 +0100

    Sheet Importer implemented

--- pkg/gateways/importers/sheetsImporter.go
+++ pkg/gateways/importers/sheetsImporter.go
@@ -2,42 +2,59 @@ package importers
 
 import (
 	"context"
+	"expenses-app/pkg/app/importing"
 	"fmt"
 	"log"
+	"time"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
 
-type sheetsImporter struct {
-	srv             *sheets.Service // DIP for better testeability. I'm not testing this though
-	credentialsPath string
-	spreadsheetId   string
+type SheetsImporter struct {
+	srv           *sheets.Service // DIP for better testeability. I'm not testing this though
+	spreadsheetId string
+	pageRange     string
 }
 
-func main() {
+func NewSheetsImporter(srv *sheets.Service, sheetID, pageRange string) *SheetsImporter {
+	return &SheetsImporter{srv, sheetID, pageRange}
+}
+
+func NewSheetService(credPath string) (*sheets.Service, error) {
 	ctx := context.Background()
-	srv, err := sheets.NewService(ctx, option.WithServiceAccountFile("credentials.json"))
+	srv, err := sheets.NewService(ctx, option.WithServiceAccountFile(credPath))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+		return nil, err
 	}
+	return srv, nil
+}
 
-	// Prints the names and majors of students in a sample spreadsheet:
-	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-	spreadsheetId := "1kZKHDpHl2ZmrkACZ6jrEIAhvC_UyQ_Qjxv2MXNYA-l0"
-	readRange := "Expenses!A2:E"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+//GetAllCategories() ([]string, error)
+func (si *SheetsImporter) GetImportedExpenses() ([]importing.ImportedExpense, error) {
+	//readRange := "Expenses!A2:E"
+	resp, err := si.srv.Spreadsheets.Values.Get(si.spreadsheetId, si.pageRange).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
 
 	if len(resp.Values) == 0 {
-		fmt.Println("No data found.")
+		log.Println("No data found")
 	} else {
-		fmt.Println("Name, Major:")
 		for _, row := range resp.Values {
-			// Print columns A and E, which correspond to indices 0 and 4.
-			fmt.Printf("%s, %s\n", row[0], row[4])
+			fmt.Println(row)
 		}
 	}
+	return []importing.ImportedExpense{
+		{
+			Amount:   1.0,
+			Currency: "euro",
+			Product:  "Wine",
+			Shop:     "Mercadona",
+			Date:     time.Now(),
+			City:     "Barcelona",
+			Town:     "Spain",
+			Category: "Alimentos",
+		}}, nil
 }
