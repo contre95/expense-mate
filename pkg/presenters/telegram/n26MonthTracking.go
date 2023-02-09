1aa71262289670c25d8169a80a3ce96d3cfd9114
contre95 <lucascontre95@gmail.com>
2023-02-09 19:13:33 +0100

    Telegram

--- pkg/presenters/telegram/n26MonthTracking.go
+++ pkg/presenters/telegram/n26MonthTracking.go
@@ -1,9 +1,11 @@
 package telegram
 
 import (
+	"encoding/csv"
 	"expenses-app/pkg/app/querying"
 	"expenses-app/pkg/app/tracking"
 	"fmt"
+	"net/http"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -33,17 +35,31 @@ func displayCategories(tbot *tgbotapi.BotAPI, chatID int64, q *querying.Service)
 }
 
 func categorizeAndCreate(tbot *tgbotapi.BotAPI, chatID int64, newUpdate tgbotapi.Update, file *tgbotapi.File, t *tracking.Service) error {
+	response, err := http.Get(file.Link(tbot.Token))
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	csvData, err := csv.NewReader(response.Body).ReadAll()
+	if err != nil {
+		return nil
+	}
+	newMsg := tgbotapi.NewMessage(chatID, "")
+	for i, record := range csvData {
+		newMsg.Text += fmt.Sprintf("%d - %s - %s - %s\n", i, record[0], record[1], record[4])
+	}
+	tbot.Send(newMsg)
 	return nil
 }
 
 func n26MonthTracking(tbot *tgbotapi.BotAPI, update *tgbotapi.Update, updates *tgbotapi.UpdatesChannel, t *tracking.Service, q *querying.Service) {
 	tbot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Starting N26 Report 📄"))
 	tbot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Please send me an N26 csv file export"))
-	// Iterate over the new updates
+	// Iterate over the new newUpdates
 	for newUpdate := range *updates {
-		chatID := update.Message.Chat.ID
-		if newUpdate.Message.Document != nil {
-			file, err := readSentFile(tbot, chatID, *update)
+		chatID := newUpdate.Message.Chat.ID
+		if newUpdate.Message.Document != nil && newUpdate.Message.Document.MimeType == "text/comma-separated-values" {
+			file, err := readSentFile(tbot, chatID, newUpdate)
 			if err != nil {
 				tbot.Send(tgbotapi.NewMessage(chatID, err.Error()))
 				break
@@ -59,8 +75,10 @@ func n26MonthTracking(tbot *tgbotapi.BotAPI, update *tgbotapi.Update, updates *t
 				break
 			}
 		} else {
-			tbot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Closing N26 Report Importer 📄"))
-			tbot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Please send me an N26 csv file export. N26 Monlty tracker stopped."))
+			msg := tgbotapi.NewMessage(newUpdate.Message.Chat.ID, "Closing N26 Report Importer 📄")
+			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+			tbot.Send(msg)
+			tbot.Send(tgbotapi.NewMessage(newUpdate.Message.Chat.ID, "Please send me an N26 csv file export. N26 Monlty tracker stopped."))
 			break
 		}
 	}
@@ -73,9 +91,7 @@ func readSentFile(tbot *tgbotapi.BotAPI, chatID int64, newUpdate tgbotapi.Update
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("Received file '%s' with ID %s", file.FilePath, fileID)
-	url := "https://api.telegram.org/file/bot" + tbot.Token + "/" + file.FilePath
-	msg.Text = fmt.Sprintf("You can download the file here: \n %s\n %s", file.Link(tbot.Token), url)
+	msg.Text = fmt.Sprint("File received 👍🏽")
 	tbot.Send(msg)
 	return &file, nil
 }
