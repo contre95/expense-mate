6074c3092a0251b718915e3ab916c0a9120c0a57
Contre <lucascontre95@gmail.com>
2024-07-07 01:41:39 +0200

    Steam on notebook

--- asd.sql
+++
@@ -1,9 +0,0 @@
-select e.id, e.amount, e.product, e.shop, e.expend_date, c.id, c.name, eu.user_id
-from expenses e
-join categories c on e.category_id = c.id
-left join expense_users eu on e.id = eu.expense_id
-where ()
-order by e.expend_date desc
-limit 35
-offset 0
-
